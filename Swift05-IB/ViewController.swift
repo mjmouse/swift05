c6740283f6c805155b6bdbdcbf19dc3f7f672879
배한상 <baehansang1@baehansang-ui-macbook-pro.local>
2016-02-08 12:16:40 +0900

    Initial Commit

---
+++ Swift05-IB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift05-IB
+//
+//  Created by 배한상 on 2016. 2. 8..
+//  Copyright © 2016년 hsang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
