06d0a4bc237b37db7a881a450c43f5c697d59715
배한상 <baehansang1@baehansang-ui-macbook-pro.local>
2016-02-08 15:24:58 +0900

    add : 5 method

--- Swift05-IB/ViewController.swift
+++ Swift05-IB/ViewController.swift
@@ -15,11 +15,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBOutlet var uiTitle01: UILabel!
+    @IBAction func clickBtn01(sender: AnyObject) {
+        self.uiTitle01.text = "Button01 Clicked"
+    }
+
+    @IBOutlet var uiTitle02: UILabel!
+    @IBAction func clickBtn02(sender: AnyObject) {
+        self.uiTitle02.text = "Button01 Clicked"
+    }
+    
+    @IBOutlet var uiTitle03: UILabel!
+    @IBAction func clickBtn03(sender: AnyObject) {
+        self.uiTitle03.text = "Button01 Clicked"
+    }
+    
+    @IBOutlet var uiTitle04: UILabel!
+    @IBAction func clickBtn04(sender: AnyObject) {
+        self.uiTitle04.text = "Button01 Clicked"
+    }
+    
+    @IBOutlet var uiTitle05: UILabel!
+    @IBAction func clickBtn05(sender: AnyObject) {
+        self.uiTitle05.text = "Button01 Clicked"
+        // test
+    }
 }
 
